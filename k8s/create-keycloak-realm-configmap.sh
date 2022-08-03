ab89e3a049220aa7f6a01d15e618f864ece771eb
Ilya Kuznetsov <ikuz2002@gmail.com>
2022-08-03 19:26:37 +0300

    refactor: Remove k8s installation and common resources, change kafka url

--- k8s/create-keycloak-realm-configmap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-kubectl create configmap timepicker-realm --from-file realm.json=../timepicker-realm.json
--- k8s/installation/01-restart-traefik.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Sometimes needed, don't know why
-
-kubectl -n kube-system scale deploy traefik --replicas 0
-kubectl -n kube-system scale deploy traefik --replicas 1
